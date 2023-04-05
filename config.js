e071136b2fa18d2450e428aca4b4d3184b35efc4
Anonymous893 <124325801+Anonymous893@users.noreply.github.com>
2023-04-05 00:55:40 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+const config = {
+    NASA_API_KEY: 'glSTPk0YHomqb4aUO1fOSIVztV97j7SXOuuvVdVh',
+  }
---
+++ controllers/user.js
@@ -0,0 +1,80 @@
+require("dotenv").config(); // load .env variables
+const { Router } = require("express"); // import router from express
+const User = require("../models/User"); // import user model
+const bcrypt = require("bcryptjs"); // bcrypt to hash passwords
+const jwt = require("jsonwebtoken"); // jwt to sign tokens
+
+const router = Router(); // create router to create route bundle
+
+//Env variables with defaults
+const { SECRET = "secret" } = process.env;
+
+// Signup route to create a new user
+router.post("/signup", async (req, res) => {
+  try {
+    // hash the password
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    // create a new user
+    const user = await User.create(req.body);
+    // send new user as response
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+// Login route to verify a user and get a token
+router.post("/login", async (req, res) => {
+  try {
+    // check if the user exists
+    const user = await User.findOne({ username: req.body.username });
+    if (user) {
+      //check if password matches
+      const result = await bcrypt.compare(req.body.password, user.password);
+      if (result) {
+        // sign token and send it in response
+        const token = await jwt.sign({ username: user.username }, SECRET);
+        res.json({ token });
+      } else {
+        res.status(400).json({ error: "password doesn't match" });
+      }
+    } else {
+      res.status(400).json({ error: "User doesn't exist" });
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+module.exports = router
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const api_key = config.NASA_API_KEY
+
+const fetchNASAData = async () => {
+  try {
+    const response = await fetch(`${url}${api_key}`)
+    const data = await response.json()
+    console.log('NASA APOD data', data)
+    displayData(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const displayData = data => {
+  document.getElementById('title').textContent = data.title
+  document.getElementById('date').textContent = data.date
+  document.getElementById('picture').src = data.hdurl
+  document.getElementById('explanation').textContent = data.explanation
+}
+
+fetchNASAData()
